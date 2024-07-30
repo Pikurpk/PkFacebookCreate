5d0dd7fd2e2aaa7da8949ec14bbec1a2bb325616
Pikurpk <mdfoysalislam785@gmail.com>
2024-07-30 16:33:46 +0600

    PkFacebookCreate.py

--- PkFacebookCreate.py
+++ PkFacebookCreate.py
@@ -4,10 +4,10 @@ print(f"""
 ❖ › Facebook:- pikachu.pk.444
 ❖ › By      :- Foysal Islam
 ┗━━━━━━━━━━━━━━━━━━━━━━━┛                """)
-print('\x1b[38;5;208m❖Pk'*20)
-print('\x1b[38;5;22m❖Pk'*20)
-print('\x1b[38;5;22m❖Pk*'20)
-print('\x1b[38;5;208m❖Pk'*20)
+print('\x1b[38;5;208m ❖Pk'*20)
+print('\x1b[38;5;22m ❖Pk'*20)
+print('\x1b[38;5;22m ❖Pk*'20)
+print('\x1b[38;5;208m ❖Pk'*20)
 import requests
 import random
 import string
